d8a285428c316be6b03d4f3b55bc5d83fa94dba6
caylynmac <152448716+caylynmac@users.noreply.github.com>
2024-05-21 11:18:01 -0600

    fixed main height

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   const linkStyles = "underline text-cyan-600 hover:text-cyan-300";
   return (
-    <main>
+    <main className="h-screen">
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p><Link className={linkStyles} href="./week-2/">Week 2</Link></p>
     </main>
